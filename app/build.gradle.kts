d73549f191ccc290ce156a60e7da7768e4fd32e1
Javier ahumada <ahumadacortesjavier@gmail.com>
2025-11-01 20:43:41 -0300

    primer Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.appiotandroidxml"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.appiotandroidxml"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures {
+        viewBinding = true   // útil para XML
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    // SplashScreen moderno (Android 12+ con compatibilidad hacia atrás)
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/example/appiotandroidxml/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.appiotandroidxml
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.appiotandroidxml.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var vb: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Instala el splash lo antes posible
+        val splash = installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+
+        // (Opcional) mantenerlo brevemente mientras cargas algo ligero
+        // splash.setKeepOnScreenCondition { false }
+
+
+        enableEdgeToEdge()
+        vb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vb.root)
+       // setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
