e4e086565d8dbe79f26fe4874440fdc3bfc5048b
Javier ahumada <ahumadacortesjavier@gmail.com>
2025-11-02 22:34:56 -0300

    actualizacion splash

--- app/src/main/java/com/example/appiotandroidxml/MainActivity.kt
+++ app/src/main/java/com/example/appiotandroidxml/MainActivity.kt
@@ -6,10 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.appiotandroidxml.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var vb: ActivityMainBinding
+    private var loading = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // Instala el splash lo antes posible
@@ -19,7 +23,14 @@ class MainActivity : AppCompatActivity() {
 
         // (Opcional) mantenerlo brevemente mientras cargas algo ligero
         // splash.setKeepOnScreenCondition { false }
+        // Mantenemos el splash mientras loading sea true
+        splash.setKeepOnScreenCondition { loading }
 
+        // Simulamos una tarea en background (p. ej., validación de login)
+        lifecycleScope.launch {
+            delay(1500) // Espera 1.5 segundos o espera real de una consulta
+            loading = false // Cuando termina, el splash desaparece automáticamente
+        }
 
         enableEdgeToEdge()
         vb = ActivityMainBinding.inflate(layoutInflater)
